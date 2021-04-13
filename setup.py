d7238e19c0c81d1abb4be1a4be7e2710f510f0be
Tom Edwards <tom@geneious.com>
2021-04-13 12:20:24 +1200

    Released a new version of the project with dependencies updated for python 3.9

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 
 setup(name='Biomatters-Azimuth',
-      version='0.2.3',
+      version='0.2.4',
       author='Nicolo Fusi and Jennifer Listgarten',
       author_email="fusi@microsoft.com, jennl@microsoft.com",
       description=("Machine Learning-Based Predictive Modelling of CRISPR/Cas9 guide efficiency"),
       packages=["azimuth", "azimuth.features", "azimuth.models", "azimuth.tests"],
       package_data={'azimuth': ['saved_models/*.*', 'data/*.*', 'tests/*.*']},
-      install_requires=['scipy', 'numpy==1.19.3', 'nose', 'scikit-learn==0.23.2', 'pandas', 'biopython', 'xlrd'],
+      install_requires=['scipy', 'numpy>=1.19.3', 'nose', 'scikit-learn==0.24.1', 'pandas', 'biopython', 'xlrd'],
       license="BSD",
       # ext_modules=cythonize("ssk_cython.pyx"),
       )
