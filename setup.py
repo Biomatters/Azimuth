dbd30b9d74f90f1846c0a31bcafcec8b36215af7
Tom Edwards <tomo12321@hotmail.com>
2022-11-22 09:58:17 +1300

    Update setup.py
    Going for a different approach of minimal changes, different OS's and python versions require different dependency versions.
    Also bumped release version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 
 setup(name='Biomatters-Azimuth',
-      version='0.2.5',
+      version='0.2.6',
       author='Nicolo Fusi and Jennifer Listgarten',
       author_email="fusi@microsoft.com, jennl@microsoft.com",
       description=("Machine Learning-Based Predictive Modelling of CRISPR/Cas9 guide efficiency"),
       packages=["azimuth", "azimuth.features", "azimuth.models", "azimuth.tests"],
       package_data={'azimuth': ['saved_models/*.*', 'data/*.*', 'tests/*.*']},
-      install_requires=['scipy==1.9.3', 'numpy==1.23.4', 'nose', 'scikit-learn==0.24.1', 'pandas==1.5.1', 'biopython==1.79', 'xlrd==2.0.1'],
+      install_requires=['scipy', 'numpy>=1.19.3', 'nose', 'scikit-learn==0.24.1', 'pandas', 'biopython<=1.79', 'xlrd'],
       license="BSD",
       # ext_modules=cythonize("ssk_cython.pyx"),
       )
